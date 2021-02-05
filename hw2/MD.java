b447944dca72fef4f8c423aa0148669886e20aa8
Tzu-Chien Huang <68072099+lenow520@users.noreply.github.com>
2021-02-05 15:43:30 +0800

    Update and rename MaxDifference4106056012.java to MD.java

--- hw2/MD.java
+++ hw2/MD.java
@@ -1,4 +1,4 @@
-public class MaxDifference4106056012 extends MaxDifference{
+public class MD extends MaxDifference{
 	public  int max_d(int[] list) {
 		int max=0,min=0;
 		for(int i=0;i<list.length;i++) {
@@ -9,7 +9,7 @@ public class MaxDifference4106056012 extends MaxDifference{
 	}
 	
 	/*public static void main(String[] args) {
-	MaxDifference4106056012 test = new MaxDifference4106056012();
+	MD test = new MD();
 	int[] A = {1,0,3,-3,6,-7,49,-78,65,13,5,-35} ;
 	int i = test.max_d(A);
 	System.out.println(i);
